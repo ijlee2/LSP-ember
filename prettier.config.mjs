159205cb073a82a270976aa920a41492e408d751
ijlee2 <ijlee2@users.noreply.github.com>
2026-09-09 20:04:23 +0200

    Initial commit

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+import baseConfiguration from '@ijlee2-frontend-configs/prettier/node';
+
+export default {
+  ...baseConfiguration,
+  overrides: [
+    ...baseConfiguration.overrides,
+    // {
+    //   files: '*.sublime-syntax',
+    //   options: {
+    //     parser: 'yaml',
+    //     printWidth: 80,
+    //   },
+    // },
+  ],
+};
